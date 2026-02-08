d37573ce35665710f381b693de4916f269a90c64
sandip84 <sandip84@hotmail.com>
2026-02-08 14:02:07 -0500

    added api change

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello sandip21!2";
+    private static final String MESSAGE = "Hello sandip21!22";
 
     public App() {}
 
