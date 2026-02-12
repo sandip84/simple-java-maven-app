41d8e47244845f40a02afdcd2380974a16a62abd
sandip84 <sandip84@hotmail.com>
2026-02-12 14:27:34 -0500

    changed

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -5,7 +5,7 @@ package com.mycompany.app;
  */
 public class App {
 
-    private static final String MESSAGE = "Hello kumar12";
+    private static final String MESSAGE = "Hello kumar123";
 
     public App() {}
 
