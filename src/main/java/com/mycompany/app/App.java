7df70c109d7a3d24ecfb32243352f14180b8a557
sandip84 <sandip84@hotmail.com>
2026-02-13 17:32:06 -0500

    we

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -1,18 +1,21 @@
 package com.mycompany.app;
 
-/**
- * Hello world!
- */
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
 public class App {
 
-    private static final String MESSAGE = "Hello kumar212";
-
-    public App() {}
+    private static final String MESSAGE = "Hello kumar212-web";
 
     public static void main(String[] args) {
-        System.out.println(MESSAGE);
+        SpringApplication.run(App.class, args);
     }
 
+    @GetMapping("/")
     public String getMessage() {
         return MESSAGE;
     }
