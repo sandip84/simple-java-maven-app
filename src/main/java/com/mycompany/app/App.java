e0a27bf9a6686dd8a5b1f7ed9d8793074a832395
sandip84 <sandip84@hotmail.com>
2026-02-15 16:55:34 -0500

    pushed for argocd

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class App {
 
-    private static final String MESSAGE = "Hello kumar212-buildah";
+    private static final String MESSAGE = "Hello argocd";
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
