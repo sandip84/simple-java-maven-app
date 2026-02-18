1b7afa60dfcbb8bf7d111ba35365e23d517466e5
sandip84 <sandip84@hotmail.com>
2026-02-17 21:00:14 -0500

    restore

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class App {
 
-    private static final String MESSAGE = "Hello argocd";
+    private static final String MESSAGE = "Hello argocd-after restore";
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
