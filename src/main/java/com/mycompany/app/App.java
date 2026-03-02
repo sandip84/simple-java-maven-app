5dac1f4098e10f4c81092a1454eefd1aa943c236
sandip84 <sandip84@hotmail.com>
2026-03-01 20:35:31 -0500

    added kustomize

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class App {
 
-    private static final String MESSAGE = "Hello argocd-after restore & Kustomize-1";
+    private static final String MESSAGE = "Hello argocd-after restore & Kustomize-2";
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
