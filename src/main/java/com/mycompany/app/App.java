627cb9b98bcaa247ea93e646efdb2cf7a587a8fc
sandip84 <sandip84@hotmail.com>
2026-03-20 20:55:43 -0400

    added efs

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class App {
 
-    private static final String MESSAGE = "Hello argocd-after restore & efs-2";
+    private static final String MESSAGE = "Hello argocd-after restore & efs-3";
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
