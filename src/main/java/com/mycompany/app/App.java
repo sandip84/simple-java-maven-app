042e0aac65d127ded3e44c635fb9379efe88a017
sandip84 <sandip84@hotmail.com>
2026-03-24 11:23:00 -0400

    test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class App {
 
-    private static final String MESSAGE = "Hello argocd-after-java-app-ingress";
+    private static final String MESSAGE = "Hello 123";
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
