cca444c87067ade0741139645aeedde324c746e8
sandip84 <sandip84@hotmail.com>
2026-03-24 14:31:24 -0400

    test

--- src/main/java/com/mycompany/app/App.java
+++ src/main/java/com/mycompany/app/App.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class App {
 
-    private static final String MESSAGE = "Hello private repo-1";
+    private static final String MESSAGE = "Hello private git app";
 
     public static void main(String[] args) {
         SpringApplication.run(App.class, args);
